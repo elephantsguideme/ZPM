6ae7040822a0e31af77ed97fcfa613ecc0a24447
lab <aa@o.pl>
2018-03-09 10:23:15 +0100

    lab2

--- Lab_01/source/main.cpp
+++ Lab_01/source/main.cpp
@@ -1,51 +1,33 @@
 #include <Windows.h>
 #include <stdio.h>
 
-int WINAPI WinMain(HINSTANCE histance, HINSTANCE hPrevinstance, PSTR szCmdLine, int iCmdShow)
-{
-  char buffer[50];
-  
-  /*
-  int iRetKey = MessageBox(0, ">20", "Aplikacja", MB_YESNO);
-  if (iRetKey == IDYES) {
-    iRetKey = MessageBox(0, ">30", "Aplikacja", MB_YESNO);
-    if (iRetKey == IDYES) {
-      iRetKey = MessageBox(0, ">35", "Aplikacja", MB_YESNO);
-      if (iRetKey == IDYES) {
-        iRetKey = MessageBox(0, ">38", "Aplikacja", MB_YESNO);
-        if (iRetKey == IDYES) {
-          iRetKey = MessageBox(0, ">39", "Aplikacja", MB_YESNO);
-          if (iRetKey == IDYES) {
-            MessageBox(0, "40", "Aplikacja", MB_OK);
-          }
-          else
-          {
-            MessageBox(0, "39", "Aplikacja", MB_OK);
 
-          }
-          }
+
+  int WINAPI WinMain(HINSTANCE histance, HINSTANCE hPrevinstance, PSTR szCmdLine, int iCmdShow)
+  {
+    char buffer[50];
+
+
+
+    int t = 1, iRetKey;
+    int n, x = 1, y = 1000;
+
+    while (t) {
+      n = sprintf_s(buffer, "> %d ?", (x + y) / 2);
+      iRetKey = MessageBox(0, buffer, "Aplikacja", MB_YESNO);
+      if (iRetKey == IDYES) {
+        x = (x + y) / 2 + 1;
+      }
+      else {
+        y = (x + y) / 2;
+      }
+      if (x == y) {
+        t = 0;
       }
     }
-  }
-  else
-  {
-    
-  }*/
-  
-  int t = 1;
-  int n, x = 20;
-  n = sprintf_s(buffer, "> %d ?", x);
-  while (t) {
-    n = sprintf_s(buffer, "> %d ?", x);
-    int iRetKey =MessageBox(0, buffer, "Aplikacja", MB_YESNO);
-    if (iRetKey == IDYES) {
-      x = (x + 40) / 2;
-    }
-    else {
-      x = x / 2;
-    }
+    sprintf_s(buffer, "Twoja liczba to %d", x);
+    MessageBox(0, buffer, "Aplikacja", MB_OK);
 
+    return 0;
   }
 
-  return 0;
-}
